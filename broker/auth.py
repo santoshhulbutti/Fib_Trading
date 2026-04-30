efc19cc3b78e9b770c81113a6daf6d81563ef1be
santoshhulbutti <santoshhulbutti@gmail.com>
2026-04-30 18:47:31 +0530

    settings.py modified, auth.py modified

--- broker/auth.py
+++ broker/auth.py
@@ -33,19 +33,5 @@ def get_access_token():
     if "access_token" not in response:
         raise Exception(f"Auth Failed: {response}")
 
-    # Here is the access token which will be used further for creating Fyers instance of your account.
-    try:
-        access_token = response["access_token"]
-        print("token: ", access_token)
-        with open("fyers_access_token.txt", 'w') as file:
-            file.write(access_token)
-        with open("fyers_client_id.txt", 'w') as file:
-            file.write(CLIENT_ID)
-
-    except Exception as e:
-        print(e, response)
-
-
     print("✅ Auth Successful")
-
     return response["access_token"]
